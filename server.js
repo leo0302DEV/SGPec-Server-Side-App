6c00f7334ed9cdcc7eb687a7ced56cac660f66fd
Leonardo Kramer Nadal <leokn0102@gmail.com>
2024-08-04 10:29:53 -0300

    Adding a start command

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv/config");
 const app = require("./src/app.js");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server running on port 3000");
