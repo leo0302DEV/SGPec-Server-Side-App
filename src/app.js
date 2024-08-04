b1f797a5d0f5bcf699b1aba8474d1e6462d53a4a
Leonardo Kramer Nadal <leokn0102@gmail.com>
2024-08-04 10:50:58 -0300

    Solving a bug on app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/index.js");
-const errorMiddleware = require("./middlewares/ErrorMiddleware.js");
+const errorMiddleware = require("./middlewares/errorMiddleware.js");
 
 const app = express();
 
